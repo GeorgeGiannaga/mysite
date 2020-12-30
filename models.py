744a394b20e2ebd0d0a66e0cdde146ebb9efa905
George Giannakopoulos <75226733+GeorgeGiannaga@users.noreply.github.com>
2020-12-30 20:33:44 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Search(models.Model):
     search = models.CharField(max_length=500)
@@ -14,12 +14,13 @@ class Search(models.Model):
 
 
 class User_insertion(models.Model):
+    user = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
     Last_name = models.CharField(max_length=50)
     First_name =models.CharField(max_length=50)
     Country = models.CharField(max_length=50)
     City = models.CharField(max_length=50)
     Address = models.CharField(max_length=50)
-    Description = models.CharField(max_length=50)
+    Description = models.CharField(max_length=200)
     objects = models.Manager()
 
     def __str__(self):
